f8d193fb47131d4f087d9859e1a2f2299b08f1b2
Allen Kim <seunkim@ea.com>
2022-09-06 21:41:05 -0700

    fix: changed network profile for aks

--- network.tf
+++ network.tf
@@ -2,4 +2,8 @@ resource "azurerm_role_assignment" "example" {
   scope                = var.aks_subnet.id
   role_definition_name = "Network Contributor"
   principal_id         = azurerm_kubernetes_cluster.aks_rs.identity.0.principal_id
+
+  depends_on = [
+    azurerm_kubernetes_cluster.aks_rs
+  ]
 }
